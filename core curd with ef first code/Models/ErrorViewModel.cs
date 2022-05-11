1c314baba7753ab4f2042e2a367424fea9b4b075
Tran-Hoc <2001tranhoc@gmail.com>
2022-05-11 21:04:50 +0700

    Add project files.

---
+++ core curd with ef first code/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace core_curd_with_ef_first_code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
